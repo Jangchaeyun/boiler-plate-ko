99c3e3854e891a67c3ce148ae8cc7dfaa1ace54a
Jangchaeyun <62280276+Jangchaeyun@users.noreply.github.com>
2022-04-03 22:18:58 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+
+const config = require("./config/key");
+
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-unlencoded
+app.use(bodyParser.urlencoded({extends: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://zzangsally:t4840779@boiler-plate.b3kz1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello world~~안녕하세요 ~'))
 
-app.listen(port, () => console.log('Example app listing on port $(port)!'))
+app.post('/register', (req, res) => {
+    //회원 가입 핳때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+app.listen(port, () => console.log('Example app listing on port ${port}!'))
